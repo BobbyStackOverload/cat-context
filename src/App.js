a4ff180876dcfd347587071ec2bce36520214f1c
Bobby W Bryant <sirbob1999@gmail.com>
2020-04-08 20:46:13 -0400

    working on context

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react';
+import { StateProvider } from './context';
+import DemoComponent from './components/DemoComponent';
+
+
+function App() {
+  const initialState = {
+    name: "Bobby",
+  };
+
+  const reducer = (state, action) => {
+    const { name } = action;
+    switch (action.type) {
+      case "CHANGE_NAME":
+        return {
+          ...state,name,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <StateProvider value={useReducer(reducer, initialState)}>
+        <DemoComponent/>
+      </StateProvider>
+    </div>
+  );
+}
+
+export default App;
